7218cd7ecb363586dee4f4cbc9ebe5384b8c85b7
Andrey <amigin@gmail.com>
2019-03-31 13:04:15 +0300

    Updated request options for Blobs

--- MyNoSqlServer.AzureStorage/AzureStorageBinder.cs
+++ MyNoSqlServer.AzureStorage/AzureStorageBinder.cs
@@ -1,4 +1,3 @@
-using Common;
 using MyNoSqlServer.Domains;
 using MyNoSqlServer.Domains.SnapshotSaver;
 
@@ -6,69 +5,20 @@ namespace MyNoSqlServer.AzureStorage
 {
     public static class AzureStorageBinder
     {
-        
-        private const char TableNamePartitionSplitter = (char) 1;
-        
-        private static string GenerateBlobName(string tableName, string partitionKey)
-        {
-            return (/*tableName +TableNamePartitionSplitter + */partitionKey).ToBase64();
-        }
-
-        public static (string tableName, string partitionKey) DecodeKey(string base64Key)
-        {
-            var pair = base64Key.Base64ToString().Split(TableNamePartitionSplitter);
-            return (pair[0], pair[1]);
-        }
-
         public static void BindAzureStorage(this string connectionString)
         {
             if (string.IsNullOrEmpty(connectionString))
                 return;
 
-
             var storageBlob = new AzureStorageBlob(connectionString);
 
-         
- 
-
             SnapshotSaverEngine.Instance = new SnapshotSaverEngine(
-
-                partitionSnapshot =>
-                {
-                    //Console.WriteLine($"{DateTime.UtcNow:s} Save snapshot {partitionSnapshot.TableName}/{partitionSnapshot.PartitionKey}. Data Length: {partitionSnapshot.Snapshot.Length}");
-                    //var blobName = GenerateBlobName(partitionSnapshot.TableName, partitionSnapshot.PartitionKey);
-                    return storageBlob.SaveToBlobAsync(partitionSnapshot.TableName, partitionSnapshot.PartitionKey, partitionSnapshot.Snapshot);
-                },
-
-                async () =>
-                {
-
-                    var snapshots = await storageBlob.LoadBlobsAsync();
-                    return snapshots;
-                    
-                    /*
-                    var files = await storageBlob.GetFilesAsync(containerName);
-                    var result = new List<PartitionSnapshot>();
-
-                    foreach (var file in files)
-                    {
-                        var data = await storageBlob.LoadBlobAsync(containerName, file);
-                        var (tableName, partitionKey) = DecodeKey(file);
-                        Console.WriteLine($"Loaded snapshot : {tableName}/{partitionKey}");
-
-                        var snapshot = PartitionSnapshot.Create(tableName, partitionKey, data);
-                        result.Add(snapshot);
-                    }
-
-                    return result;
-                    */
-                }
-
-            );
+                partitionSnapshot => storageBlob.SaveToBlobAsync(partitionSnapshot.TableName, partitionSnapshot.PartitionKey, partitionSnapshot.Snapshot),
+                async () => await storageBlob.LoadBlobsAsync());
 
             ServiceLocator.SnapshotSaverEngine = SnapshotSaverEngine.Instance;
             SnapshotSaverEngine.Instance.Start();
         }
-
     }
+    
 }
--- MyNoSqlServer.AzureStorage/AzureStorageBlob.cs
+++ MyNoSqlServer.AzureStorage/AzureStorageBlob.cs
@@ -15,12 +15,21 @@ namespace MyNoSqlServer.AzureStorage
     {
         private readonly CloudStorageAccount _storageAccount;
         private readonly TimeSpan _maxExecutionTime = TimeSpan.FromSeconds(10);
-        
+
+
+        private BlobRequestOptions _requestOptions;
+
         public AzureStorageBlob(string connectionString)
         {
             _storageAccount = CloudStorageAccount.Parse(connectionString);
+
+            _requestOptions =
+                new BlobRequestOptions
+                {
+                    MaximumExecutionTime = _maxExecutionTime
+                };
         }
-        
+
         private CloudBlobContainer GetContainerReference(string container)
         {
             NameValidator.ValidateContainerName(container);
@@ -29,55 +38,18 @@ namespace MyNoSqlServer.AzureStorage
             return blobClient.GetContainerReference(container.ToLower());
         }
         
-        private BlobRequestOptions GetRequestOptions()
-        {
-            return new BlobRequestOptions
-            {
-                MaximumExecutionTime = _maxExecutionTime
-            };
-        }
         
-        private readonly Dictionary<string, CloudBlobContainer> _containers = new Dictionary<string, CloudBlobContainer>();
-        
-        private async ValueTask<CloudBlobContainer> GetBlockBlobReferenceAsync(string container, string key, bool anonymousAccess, bool createIfNotExists)
-        {
-            NameValidator.ValidateBlobName(key);
-
-            lock (_containers)
-            {
-                if (_containers.ContainsKey(container))
-                    return _containers[container];
-            }
-
-            var containerRef = GetContainerReference(container);
-
-            if (createIfNotExists)
-            {
-                await containerRef.CreateIfNotExistsAsync(GetRequestOptions(), null);
-            }
-            if (anonymousAccess)
-            {
-                var permissions = await containerRef.GetPermissionsAsync(null, GetRequestOptions(), null);
-                permissions.PublicAccess = BlobContainerPublicAccessType.Container;
-                await containerRef.SetPermissionsAsync(permissions, null, GetRequestOptions(), null);
-            }
-
-            lock (_containers)
-            {
-                if (!_containers.ContainsKey(container))
-                    _containers.Add(container, containerRef);
-            }
-            
-            return containerRef;
-        }        
-
         
         private async ValueTask<CloudBlobContainer> GetBlockBlobReferenceAsync(string container)
         {
             var containerRef = GetContainerReference(container);
-
+            
             if (!await containerRef.ExistsAsync())
                 return null;
+            
+            var permissions = await containerRef.GetPermissionsAsync(null, _requestOptions, null);
+            permissions.PublicAccess = BlobContainerPublicAccessType.Container;
+            await containerRef.SetPermissionsAsync(permissions, null, _requestOptions, null);
             
             return containerRef;
         } 
@@ -163,35 +135,14 @@ namespace MyNoSqlServer.AzureStorage
                     
                     result.Add(snapshot);
                     
-                    
                     Console.WriteLine("Loaded snapshot: "+snapshot);
                 }
            
             }
 
-
             return result;
-
         }        
-
-
-        public async ValueTask<IEnumerable<string>> GetFilesAsync(string containerName)
-        {
-            var blobRef = GetContainerReference(containerName);
-
-            var dir = blobRef.GetDirectoryReference("");
-
-            BlobContinuationToken continuationToken = null;
-            var results = new List<CloudBlockBlob>();
-            do
-            {
-                var response = await dir.ListBlobsSegmentedAsync(continuationToken);
-                continuationToken = response.ContinuationToken;
-                results.AddRange(response.Results.Cast<CloudBlockBlob>());
-            } while (continuationToken != null);
-
-            return results.Select(x => x.Name).ToList();
-        }
+        
         
     }
     
