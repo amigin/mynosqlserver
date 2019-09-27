9c09a63346ce5967ebfafdd6bf6384d3368f24ac
amigin <amigin@gmail.com>
2019-09-27 21:32:17 +0300

    Updated Version of MyNoSql

--- MyNoSqlServer.Api/Startup.cs
+++ MyNoSqlServer.Api/Startup.cs
@@ -23,17 +23,14 @@ namespace MyNoSqlServer.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            
-            
-            
+
+
+
             services.AddApplicationInsightsTelemetry(Configuration);
-            
-            services.AddMvc(o =>
-                {
-                    o.EnableEndpointRouting = false;
-                })
+
+            services.AddMvc(o => { o.EnableEndpointRouting = false; })
                 .AddNewtonsoftJson();
-            
+
             /*
             services.Configure<ForwardedHeadersOptions>(options =>
             {
@@ -42,31 +39,31 @@ namespace MyNoSqlServer.Api
             });
 
             */
-            
-            services.AddSignalR(); 
-            
-            
+
+            services.AddSignalR();
+
+
             services.AddSwaggerDocument();
 
             // Register the Swagger generator, defining 1 or more Swagger documents
-   //         services.AddSwaggerGen(c =>
-   //         {
-   //             c.SwaggerDoc("v1", new OpenApiInfo
-    //            {
-    //                Title = "MyNoSql API",
-    //                Version = "1.0"
+            //         services.AddSwaggerGen(c =>
+            //         {
+            //             c.SwaggerDoc("v1", new OpenApiInfo
+            //            {
+            //                Title = "MyNoSql API",
+            //                Version = "1.0"
 
 //                });
- //           });
+            //           });
 
-            
+
             var settings = SettingsLoader.LoadSettings();
-            
+
             settings.BackupAzureConnectString.BindAzureStorage();
 
             ServiceLocator.DataSynchronizer = new ChangesPublisherToSignalR();
-            
-            
+
+
             ServiceLocator.SnapshotSaverEngine.Start();
 
         }
@@ -74,46 +71,43 @@ namespace MyNoSqlServer.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostApplicationLifetime applicationLifetime)
         {
-            
+
             // Enable middleware to serve generated Swagger as a JSON endpoint.
-     //      app.UseSwagger();
+            //      app.UseSwagger();
 
             // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
             // specifying the Swagger JSON endpoint.
-    //       app.UseSwaggerUI(c =>
-    //        {
-     //           c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyNoSqlServer API V1");
-     //       });            
+            //       app.UseSwaggerUI(c =>
+            //        {
+            //           c.SwaggerEndpoint("/swagger/v1/swagger.json", "MyNoSqlServer API V1");
+            //       });            
 
             //app.UseHttpsRedirection();
 
 
             applicationLifetime.ApplicationStopping.Register(OnShutdown);
-            
+
             app.Use((context, next) =>
             {
                 if (context.Request.Headers.ContainsKey("X-Forwarded-Proto"))
                     context.Request.Scheme = context.Request.Headers["X-Forwarded-Proto"];
                 return next();
             });
-            
-          //  app.UseForwardedHeaders();
 
-            
+            //  app.UseForwardedHeaders();
+
+
             app.UseStaticFiles();
-            
-                 app.UseSwagger();
-                app.UseSwaggerUi3();
-            
-            app.UseMvc();
-            
-            
-            app.UseSignalR(routes =>
-            {
-                routes.MapHub<ChangesHub>("/changes");
-            });
 
-            
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+
+            app.UseMvc();
+
+
+            app.UseSignalR(routes => { routes.MapHub<ChangesHub>("/changes"); });
+
+
         }
 
         private void OnShutdown()
